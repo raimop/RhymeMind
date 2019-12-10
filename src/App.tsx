659ab3a50ea8b1af4836ad7571978dcb4793eeb2
raimop <ray@tlu.ee>
2019-12-10 16:00:16 +0200

    Storm over?

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,77 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
 import './App.css';
-import Game from './pages/Game';
-import StartScreen from './pages/StartScreen';
 
-interface IProps {}
-
-interface IState {}
-
-class App extends React.Component<IProps, IState> {
-  render() {
-    return (
-    <Router>
-      <div>
-        <Link to="/">StartScreen</Link> 
-        <Link to="/game">Game</Link>
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <StartScreen/>
-          </Route>
-          <Route path="/game">
-            <Game/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    )
+class App extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
   };
+  
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+  
+  handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    
+    this.setState({ responseToPost: body });
+  };
+  
+render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
