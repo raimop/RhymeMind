0cd1f8149c84a7157292171dbba5d6a9a86a315e
raimop <ray@tlu.ee>
2019-12-11 12:29:29 +0200

    Clearing up unnecessary files

--- server/server.js
+++ server/server.js
@@ -8,18 +8,6 @@ const { returnRhyme, compareRhymes, returnWords } = require("./rhyming");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API calls
-/* app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-}); */
-
 app.get("/api/rhyme/:sentence", (req, res) => {
   return res.send(returnRhyme(req.params.sentence));
 });
