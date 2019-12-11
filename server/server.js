9f7aa3150941a6df0e6a1e073d95bb2206723a7a
raimop <ray@tlu.ee>
2019-12-11 12:15:34 +0200

    Nice to meet you, I am Dumdum

--- server/server.js
+++ server/server.js
@@ -34,11 +34,11 @@ app.get("/api/words/:word", (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, '../build')));
     
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 }
 
